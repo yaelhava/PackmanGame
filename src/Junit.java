acf9906905eafda39f38a96ac3e6bebbf6be59dd
yael hava <yaelhava2610@gmail.com>
2018-12-23 23:52:44 +0200

    add diagram and Junit

--- src/Junit.java
+++ src/Junit.java
@@ -1,12 +1,24 @@
 import static org.junit.jupiter.api.Assertions.*;
 
+
 import org.junit.jupiter.api.Test;
+
+import Geom.Point3D;
+import TheGame.Map;
 
 class Junit {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void GPS2Pixeltest() {
+		Point3D p1= new Point3D(35.212418,32.105765);
+		Point3D ans= new Point3D(1433,0);
+		Map m= new Map("Ariel1.png");
+		Point3D p2=m.GPS2Pixel(p1, 1433, 642);
+		System.out.println(p2);
+		if(p2.x()!=446708.1062008713||p2.y()!=-512585.76869753737) {
+			fail("the GPS2Pixel function dosent work well");
+		}
+		
 	}
 
 }
